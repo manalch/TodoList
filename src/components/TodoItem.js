901e5220fcf2672a817950029013a68ba52c43ca
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-27 18:13:19 +0800

    [TodoList][Cedric-JC]feature: used changeStatus with this.props.todo.id in todoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
+
   render() {
+    const style = {
+      textDecoration: this.props.todo.done ? 'line-through' : ''
+    }
     return (
-      <div id="todoItem">{this.props.todo.text}<span onClick={() => this.props.deleteTodo(this.props.todo.id)}>X</span></div>
+      <div id="todoItem" onClick={() => this.props.changeStatus(this.props.todo.id)}>
+        <span style={style}>{this.props.todo.text}</span>
+        <span onClick={() => this.props.deleteTodo(this.props.todo.id)}>X</span>
+      </div>
     );
   }
 }
