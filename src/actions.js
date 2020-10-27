5dee5c201bd6b07e93106c3d16290ce1655c1ffa
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-27 18:11:14 +0800

    [TodoList][Cedric-JC]feature: added changeStatus action

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,7 @@ export const addTodo = (todo) => {
 export const deleteTodo = (id) => {
   return { type: "DELETE_TODO", payload: id };
 }
+
+export const changeStatus = (id) => {
+  return { type: "CHANGE_STATUS", payload: id };
+}
