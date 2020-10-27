ca8e8156be19fbd40020d46ef15d5165aaa86aac
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-27 17:42:01 +0800

    [TodoList][Cedric-JC]feature: added deleteTodo in onClick of todoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class TodoItem extends Component {
   render() {
     return (
-      <div id="todoItem">{this.props.todo.text}<span>X</span></div>
+      <div id="todoItem">{this.props.todo.text}<span onClick={() => this.props.deleteTodo(this.props.todo.id)}>X</span></div>
     );
   }
 }
