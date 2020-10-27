09f51a9a612ebf5bafeee493ac088c26a37cce50
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-27 16:53:07 +0800

    [TodoList][Cedric-JC]feature: added onSubmit event to add todoItem

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 class TodoGenerator extends Component {
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const text = event.target.todoInput.value;
+    const id = uuidv4();
+    const todo = { id: id, text: text, done: false };
+
+    this.props.addTodo(todo);
+  }
+
   render() {
     return (
       <div id="todoGenerator">
-        <span>
-          <input type="text" name="todoInput" id="todoInput" />
-          <input type="button" value="Add Item" />
-        </span>
+        <form onSubmit={this.onSubmit}>
+          <span>
+            <input type="text" name="todoInput" id="todoInput" />
+            <input type="submit" value="Add Item" />
+          </span>
+        </form>
       </div>
     );
   }
