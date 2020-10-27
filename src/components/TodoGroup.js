784333d13336ce69a991f577f3158d72a6d55c06
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-27 17:03:54 +0800

    [TodoList][Cedric-JC]feature: added todos

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -3,9 +3,12 @@ import TodoItem from './TodoItem';
 
 class TodoGroup extends Component {
   render() {
+    const todos = this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} />
+    ));
     return (
       <div>
-        <TodoItem />
+        {todos}
       </div>
     );
   }
