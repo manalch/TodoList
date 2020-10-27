dc95aa158bf26ff21bdc57fc9d7302ae8327f4a2
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-27 19:15:49 +0800

    [TodoList][Cedric-JC]refactor: added alert when input is empty

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -6,10 +6,13 @@ class TodoGenerator extends Component {
   onSubmit = (event) => {
     event.preventDefault();
     const text = event.target.todoInput.value;
-    const id = uuidv4();
-    const todo = { id, text, done: false };
-
-    this.props.addTodo(todo);
+    if(text !== ""){
+      const id = uuidv4();
+      const todo = { id, text, done: false };
+      this.props.addTodo(todo);
+    }else{
+      alert("Empty Input!");
+    }
   }
 
   render() {
