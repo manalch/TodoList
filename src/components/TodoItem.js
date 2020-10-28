e53ab346e122ed0088b4fd9294dea2a278194260
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 15:57:54 +0800

    [Axios][Cedric-Joseph] refactor: Removed Toast in Todo Item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
 import {deleteTodo, getTodos, updateTodo } from '../apis/todo';
 
 
@@ -17,16 +15,8 @@ class TodoItem extends Component {
           this.props.getTodos(response.data);
           if (this.props.todo.done) {
             alert(this.props.todo.text + " is marked as unfinished.");
-            // toast.error(this.props.todo.text + " is marked as unfinished.", {
-            //   autoClose: 1500,
-            //   closeOnClick: true
-            // });
           } else {
             alert(this.props.todo.text + " is marked as finished.");
-            // toast.success(this.props.todo.text + " is marked as finished.", {
-            //   autoClose: 1500,
-            //   closeOnClick: true
-            // });
           }
         });
       });
@@ -36,10 +26,7 @@ class TodoItem extends Component {
       deleteTodo(id).then(response => {
         getTodos().then(response => {
           this.props.getTodos(response.data);
-          toast.success("Successfully removed " + this.props.todo.text + ".", {
-            autoClose: 1500,
-            closeOnClick: true
-          });
+          alert("Successfully removed " + this.props.todo.text + ".");
         })
       });
     }
@@ -54,7 +41,6 @@ class TodoItem extends Component {
           <span style={style} className="col-92" onClick={onClick}>{this.props.todo.text}</span>
           <span className="col-8" onClick={() => onDelete(this.props.todo.id)}><span id="deleteIcon">X</span></span>
         </div>
-        <ToastContainer />
       </div>
     );
   }
