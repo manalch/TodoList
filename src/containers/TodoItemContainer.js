a3f20402311163292c5285119c2be3bd7941154f
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 15:43:06 +0800

    [Axios][Cedric-Joseph] feature: added getTodos in TodoItemContainer

--- src/containers/TodoItemContainer.js
+++ src/containers/TodoItemContainer.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
-import { deleteTodo, changeStatus } from '../actions';
+import { deleteTodo, changeStatus, getTodos } from '../actions';
 import TodoItem from '../components/TodoItem';
 
 const mapDispatchToProps = dispatch => ({
   deleteTodo: (id) => { dispatch(deleteTodo(id)) },
-  changeStatus: (id) => { dispatch(changeStatus(id)) }
+  changeStatus: (id) => { dispatch(changeStatus(id)) },
+  getTodos: (todos) => { dispatch(getTodos(todos)) },
 });
 
 const TodoItemContainer = connect(null, mapDispatchToProps)(TodoItem);
