b608f249a803f56f6be44395320665e06080e123
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 12:57:02 +0800

    [Router][Cedric-Joseph] feature: Added Done link in TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import TodoGeneratorContainer from '../containers/TodoGeneratorContainer';
 import TodoGroupContainer from '../containers/TodoGroupContainer';
+import {Link } from 'react-router-dom';
 
 class TodoList extends Component {
   render() {
     return (
       <div>
         <h1 id="appName">TodoList</h1>
+        <Link to="/done" className="white link">done</Link>
         <TodoGeneratorContainer/>
         <TodoGroupContainer/>
       </div>
