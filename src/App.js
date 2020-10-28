59d40316c4a4e232400824ea7e7b0ef7ac76442f
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 12:56:39 +0800

    [Router][Cedric-Joseph] feature: implemented BrowserRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import DoneListContainer from './containers/DoneListContainer';
+import {HashRouter, BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoList/>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/done" component={DoneListContainer} />
+            <Route path="/" component={TodoList} />
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
