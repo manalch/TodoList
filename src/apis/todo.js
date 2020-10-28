d18c0d25a2ce62ce4867b88ff9279c416f850d80
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 15:25:40 +0800

    [Axios][Cedric-Joseph] feature: Added addTodo api

--- src/apis/todo.js
+++ src/apis/todo.js
@@ -3,3 +3,9 @@ import api from './api';
 export const getTodos = () => {
     return api.get("/api/todos");
 }
+
+export const addTodo = (text) => {
+    return api.post("/api/todos", {
+        text
+    });
+}
