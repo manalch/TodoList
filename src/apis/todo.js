d679270cfd080526f9d098c2a291af66cc662b29
MANALCH <manalch@oocl.com>
2020-10-29 22:08:31 +0800

    [spring-todo-api][joseph] feature: use backend to integrate

--- src/apis/todo.js
+++ src/apis/todo.js
@@ -1,19 +1,19 @@
 import api from './api';
 
 export const getTodos = () => {
-    return api.get("/api/todos");
+    return api.get("/todos");
 }
 
 export const addTodo = (text) => {
-    return api.post("/api/todos", {
+    return api.post("/todos", {
         text
     });
 }
 
 export const deleteTodo = (id) => {
-    return api.delete("/api/todos/" + id);
+    return api.delete("/todos/" + id);
 }
 
 export const updateTodo = (id, done) => {
-    return api.put("/api/todos/" + id, { done });
+    return api.put("/todos/" + id, { done });
 }
